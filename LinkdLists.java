13abed6447c5b320b1c68fc089bc06abe755d871
izayed9 <izayed9@gmail.com>
2023-12-06 00:46:11 +0600

    linklist full

--- LinkdLists.java
+++ LinkdLists.java
@@ -1,5 +1,21 @@
 public class LinkdLists {
-    public static void main(String[] args) {
-        
+    /**
+     * Node
+     */
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
     }
+
+    // add - first and last
+
+    public static void main(String[] args) {
+
+    }
+
 }
