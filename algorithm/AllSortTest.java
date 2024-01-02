ce6d7fc7b55212f9d50163afafdc87ec2f460d65
izayed9 <izayed9@gmail.com>
2024-01-02 11:24:20 +0600

    selection sort fixed

--- algorithm/AllSortTest.java
+++ algorithm/AllSortTest.java
@@ -10,17 +10,18 @@ public class AllSortTest {
     public static void main(String[] args) {
         int arr[] = {7,8,3,1,2};
        
-        // Bubble sort 
-        for(int i=0; i<arr.length-1; i++){
-            System.out.println("test First outside "+ i);
-            for(int j=0; j<arr.length-i-1; j++){
-              System.out.println("'test inside :'"+ j);
-              if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-              }
+        // Selection sort 
+        for(int i=0;i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){
+               if(arr[smallest]>arr[j]){
+                   smallest=j;
+               }
             }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+
         }
         printArra(arr);
     }
