9509b683363437ae458b0e0700ee514563f0d182
izayed9 <izayed9@gmail.com>
2024-01-17 18:16:01 +0600

    init activeeted

--- algolia/algoliaTesx.java
+++ algolia/algoliaTesx.java
@@ -1,6 +1,9 @@
 package algolia;
 
 public class algoliaTesx {
+     public static void init(int arr[]){
+           int a[] = {1,2,3,4,5};
+     }
      public static void main(String[] args) {
         
      }
